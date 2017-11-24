91d403af71c80aaba0468ea8ac378f2d0721e30f
vladislav.voinov <vladislav.voinov@ontariomd.com>
2017-11-24 14:24:38 -0500

    filter is working and is based on a separate reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
-import {} from '../constants/ActionTypes'
+import { ADD_TODO, DELETE_TODO, UPDATE_TODO, SET_VISIBILITY_FILTER } from '../constants/ActionTypes'
 let nextTodoId = 1
 
 export const addTodo = newItem => {
   return {
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     id: nextTodoId++,
     item: newItem
   }
@@ -11,7 +11,14 @@ export const addTodo = newItem => {
 
 export const toggleTodo = id => {
   return {
-    type: 'UPDATE_TODO',
+    type: UPDATE_TODO,
     id
+  }
+}
+
+export const setVisibilityFilter = (filter) => {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    filter
   }
 }
--- src/ui/TodoFilter.js
+++ src/ui/TodoFilter.js
@@ -1,9 +1,27 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Nav, NavItem } from 'react-bootstrap'
 
 export default class TodoFilter extends Component {
-  render () {
+
+  constructor(props) {
+    super(props)
+    this.handleSelect = this.handleSelect.bind(this)
+  }
+
+  handleSelect(selectedKey) {
+    console.log('selected->', selectedKey)
+    this.props.onTodoFilter(selectedKey)
+  }
+
+  render() {
     return (
-      <div>I am TodoFilter</div>
+      <div>
+        <Nav bsStyle="pills" activeKey={this.props.visibilityFilter} onSelect={this.handleSelect}>
+          <NavItem eventKey={'SHOW_ALL'} title="All">All</NavItem>
+          <NavItem eventKey={'SHOW_COMPLETED'} title="Completed">Completed</NavItem>
+          <NavItem eventKey={'SHOW_ACTIVE'} title="Active">Active</NavItem>
+        </Nav>
+      </div>
     )
   }
 }
